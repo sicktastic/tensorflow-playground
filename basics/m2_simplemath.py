48b55ac9eb2f1ea735d5c4125cae324cf37dee08
Anthony Lee <antwonlee@gmail.com>
2017-11-08 10:15:36 -0600

    Add basic math with tensorflow

---
+++ basics/m2_simplemath.py
@@ -0,0 +1,23 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
+
+import tensorflow as tf
+
+a = tf.constant(6.5, name="constant_a")
+b = tf.constant(3.4, name="constant_b")
+c = tf.constant(3.0, name="constant_c")
+d = tf.constant(100.2, name="constant_d")
+
+square = tf.square(a, name="square_a")
+power = tf.pow(b, c, name="power_b_c")
+sqrt = tf.sqrt(d, name="sqrt_d")
+
+
+final_sum = tf.add_n([square, power, sqrt], name="final_sum")
+
+sess = tf.Session()
+
+print("Square of a: ", sess.run(square))
+print("Power of a: ", sess.run(power))
+print("Square root of d: ", sess.run(sqrt))
+print("Sum of squre, power and square root ", sess.run(final_sum))
